45dd542a77c5b7f74f054b3d31c4be9113b2337b
varisd <varis@ufal.mff.cuni.cz>
2020-03-24 17:07:13 +0100

    modified data preprocessing

--- create_input_files.py
+++ create_input_files.py
@@ -1,10 +1,25 @@
+import argparse
 from utils import create_input_files
 
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="TODO")
+
+    parser.add_argument(
+        "--karpathy_json", default="data/caption_datasets/dataset_coco.json",
+        help="JSON containing Karpathy splits")
+    parser.add_argument("--output_dir", required=True, help="output_directory")
+
+    args = parser.parse_args()
+    return args
+
 if __name__ == '__main__':
+    args = parse_args()
+
     # Create input files (along with word map)
     create_input_files(dataset='coco',
-                       karpathy_json_path='data/caption_datasets/dataset_coco.json',
+                       karpathy_json_path=args.karpathy_json,
                        captions_per_image=5,
                        min_word_freq=5,
-                       output_folder='final_dataset',
+                       output_folder=args.output_dir,
                        max_len=50)
--- datasets.py
+++ datasets.py
@@ -5,6 +5,16 @@ import json
 import os
 
 
+def load_vocab(filepath):
+    idx2str = []
+    with open(filepath, "r") as fh:
+        for line in fh:
+            idx2str.append(line.strip())
+
+    str2idx = {x: i for i, x in enumerate(idx2str)}
+    return idx2str, str2idx
+
+
 class CaptionDataset(Dataset):
     """
     A PyTorch Dataset class to be used in a PyTorch DataLoader to create batches.
@@ -21,10 +31,14 @@ class CaptionDataset(Dataset):
         assert self.split in {'TRAIN', 'VAL','TEST'}
 
         # Open hdf5 file where images are stored
-        self.train_hf = h5py.File(data_folder + '/train36.hdf5', 'r')
+        self.train_hf = h5py.File(data_folder + '/train100.hdf5', 'r')
         self.train_features = self.train_hf['image_features']
-        self.val_hf = h5py.File(data_folder + '/val36.hdf5', 'r')
+        self.train_classes = self.train_hf["classes"]
+        self.train_attributes = self.train_hf["attributes"]
+        self.val_hf = h5py.File(data_folder + '/val100.hdf5', 'r')
         self.val_features = self.val_hf['image_features']
+        self.val_classes = self.val_hf["classes"]
+        self.val_attributes = self.val_hf["attributes"]
 
         # Captions per image
         self.cpi = 5
@@ -55,20 +69,24 @@ class CaptionDataset(Dataset):
         # Load bottom up image features
         if objdet[0] == "v":
             img = torch.FloatTensor(self.val_features[objdet[1]])
+            cls = torch.LongTensor(self.val_classes[objdet[1]])
+            attr = torch.LongTensor(self.val_attributes[objdet[1]])
         else:
             img = torch.FloatTensor(self.train_features[objdet[1]])
+            cls = torch.LongTensor(self.train_classes[objdet[1]])
+            attr = torch.LongTensor(self.train_attributes[objdet[1]])
 
         caption = torch.LongTensor(self.captions[i])
 
         caplen = torch.LongTensor([self.caplens[i]])
         
         if self.split is 'TRAIN':
-            return img, caption, caplen
+            return img, caption, caplen, cls, attr
         else:
             # For validation of testing, also return all 'captions_per_image' captions to find BLEU-4 score
             all_captions = torch.LongTensor(
                 self.captions[((i // self.cpi) * self.cpi):(((i // self.cpi) * self.cpi) + self.cpi)])
-            return img, caption, caplen,all_captions
+            return img, caption, caplen, cls, attr, all_captions
 
     def __len__(self):
         return self.dataset_size
